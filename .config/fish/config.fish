b8dbc34290a422088bdd56ed0a968ef6fd304665
ckidney-beans <ckidney@gmail.com>
2025-04-03 19:40:01 -0230

    first upload of dotfiles

---
+++ .config/fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+oh-my-posh init fish | source
+eval "$(oh-my-posh init fish --config /home/ckidney/posh-themes/themes/lambdageneration.omp.json)"
+
+fastfetch -l ARCHlabs
+
---
+++ .config/fish/functions/clear.fish
@@ -0,0 +1,4 @@
+function clear --description 'alias clear=clear && fastfetch -l Archcraft2'
+ command clear && fastfetch -l Archcraft2 $argv
+        
+end
